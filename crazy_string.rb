cfddb28e9ece19e9af805b552c57a794a1975107
01Divergent <iphone.claudia@icloud.com>
2018-07-10 18:14:08 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 # write your method here
 
-def crazy_strings(1st_string, 2nd_string)
+def crazy_string(1st_string, 2nd_string)
   1st_string = 1st_string.reverse.upcase
   2nd_string = 2nd_string.swapcase.gsub("s","z")
   return "#{1st_string} #{2nd_string}"
